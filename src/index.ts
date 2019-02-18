f7c07a7b64b414ccc0b63e8d63c5930ae18119f6
Ulysse Carion <ulyssecarion@gmail.com>
2019-02-17 19:50:45 -0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Validator, ValidatorConfig } from "./Validator";
+export { ValidationResult } from "./ValidationResult";
+export { default as InvalidSchemaError } from "./InvalidSchemaError";
+export { default as MissingURIsError } from "./MissingURIsError";
+export { default as StackOverflowError } from "./StackOverflowError";
